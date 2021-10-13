ceefd3b6cb2175266ffe49a600f9fd5d8322a2fd
haolv <haolv@solazu.com>
2021-10-13 09:37:49 +0700

    create UI inventory-management

---
+++ src/App.js
@@ -0,0 +1,165 @@
+import React, { useEffect, useState } from 'react'
+import ExcelJS from 'exceljs'
+import moment from 'moment'
+import axios from 'axios'
+import TextAreaAutoSize from 'react-textarea-autosize'
+import './styles.css'
+
+const api = axios.create({
+	baseURL: 'http://192.168.0.101:3001',
+})
+
+const CELL_WIDTH_LIST = [0, 100, 200, 200, 200, 200, 300, 300]
+
+const App = () => {
+	const [file, setFile] = useState(null)
+	const [data, setData] = useState([])
+
+	const handleChange = (e) => {
+		setFile(e.target.files[0])
+	}
+
+	const handleImport = () => {
+		const wb = new ExcelJS.Workbook()
+		const reader = new FileReader()
+
+		reader.readAsArrayBuffer(file)
+		reader.onload = () => {
+			const buffer = reader.result
+			wb.xlsx.load(buffer).then(workbook => {
+				console.log(workbook, 'workbook instance')
+				// workbook.eachSheet((sheet, id) => {
+				// 	sheet.eachRow((row, rowIndex) => {
+				// 		console.log(row.values, rowIndex)
+				// 	})
+				// })
+				const startRow = 4
+				const d = workbook.worksheets[0].getRows(startRow, workbook.worksheets[0].actualRowCount).map(r => r.values)
+				api.post('save', {
+					filename: 'test',
+					data: d,
+				}).then((res) => { alert(JSON.stringify(res)) }).catch(e => alert('error: ' + JSON.stringify(e)))
+				setData(d.map(row => {
+					row = row.map(cell => ({ editing: false, value: cell }))
+					return row
+				}))
+				console.log('rows: ', d)
+			})
+		}
+	}
+
+	const handleKeyDown = (event) => {
+		if ((event.which == 13)) {
+			event.preventDefault();
+			return false;
+		}
+	}
+
+	useEffect(() => {
+		// document.addEventListener("keydown", handleKeyDown)
+
+		// return () => {
+		// 	document.removeEventListener("keydown", handleKeyDown)
+		// }
+	}, [])
+
+	const renderDataHeader = () => {
+		return (
+			<div style={{ display: 'flex', flexDirection: 'row', backgroundColor: '#8eb3ed', marginBottom: 4 }}>
+				<div>
+					<div style={{ display: 'flex', width: CELL_WIDTH_LIST[1], height: '100%', justifyContent: 'center', borderRight: '5px solid #fff' }}>
+						<div style={{ fontSize: 18, fontWeight: 'bold', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap' }}>STT</div>
+					</div>
+				</div>
+				<div>
+					<div style={{ display: 'flex', width: CELL_WIDTH_LIST[2], height: '100%', justifyContent: 'center', borderRight: '5px solid #fff' }}>
+						<div style={{ fontSize: 18, fontWeight: 'bold', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap' }}>Ngày phát sinh</div>
+					</div>
+				</div>
+				<div>
+					<div style={{ display: 'flex', width: CELL_WIDTH_LIST[3], height: '100%', justifyContent: 'center', borderRight: '5px solid #fff' }}>
+						<div style={{ fontSize: 18, fontWeight: 'bold', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap' }}>Tên KH/Dự án</div>
+					</div>
+				</div>
+				<div>
+					<div style={{ display: 'flex', width: CELL_WIDTH_LIST[4], height: '100%', justifyContent: 'center', borderRight: '5px solid #fff' }}>
+						<div style={{ fontSize: 18, fontWeight: 'bold', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap' }}>Hãng</div>
+					</div>
+				</div>
+				<div>
+					<div style={{ display: 'flex', width: CELL_WIDTH_LIST[5], height: '100%', justifyContent: 'center', borderRight: '5px solid #fff' }}>
+						<div style={{ fontSize: 18, fontWeight: 'bold', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap' }}>KH của sale</div>
+					</div>
+				</div>
+				<div>
+					<div style={{ display: 'flex', width: CELL_WIDTH_LIST[6] + CELL_WIDTH_LIST[7], height: '50%', justifyContent: 'center', borderBottom: '5px solid #fff' }}>
+						<div style={{ fontSize: 18, fontWeight: 'bold', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap' }}>Tên công việc</div>
+					</div>
+					<div>
+						<div style={{ display: 'flex', flexDirection: 'row', width: CELL_WIDTH_LIST[6] + CELL_WIDTH_LIST[7], height: '50%', justifyContent: 'center' }}>
+							<div style={{ display: 'flex', width: CELL_WIDTH_LIST[6], height: '100%', justifyContent: 'center', borderRight: '5px solid #fff' }}>
+								<div style={{ fontSize: 18, fontWeight: 'bold', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap' }}>Hoàn thành</div>
+							</div>
+							<div style={{ display: 'flex', width: CELL_WIDTH_LIST[7], height: '100%', justifyContent: 'center' }}>
+								<div style={{ fontSize: 18, fontWeight: 'bold', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap' }}>Đang xử lý</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div>
+			<div style={{ justifyContent: 'center', marginBottom: 8 }}>
+				Working {new Date().toLocaleDateString()}
+				<input style={{ marginLeft: 50 }} type="file" onChange={handleChange} />
+				<button onClick={handleImport}>Import</button>
+			</div>
+			<div>
+				{renderDataHeader()}
+				{data.map(row => (
+					<div style={{ display: 'flex', flexDirection: 'row', backgroundColor: '#8eb3ed', marginBottom: 4 }}>
+						{[1, 2, 3, 4, 5, 6, 7].map((col) => (
+							<div>
+								<div style={{ display: 'flex', width: CELL_WIDTH_LIST[col], height: '100%', marginRight: 5 }} onDoubleClick={() => {
+									if (!row[col]) row[col] = {}
+									row[col].editing = true
+									setData([...data])
+								}}>
+									{
+										row[col]?.editing ? (
+											<TextAreaAutoSize
+												style={{ width: '100%', fontSize: 18, lineHeight: '20px', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap', overflow: 'hidden', resize: 'vertical', fontFamily: "Times New Roman" }}
+												value={row[col]?.value || ''}
+												autoFocus
+												onFocus={(event) => { event.target.setSelectionRange(row[col].value?.length, row[col].value?.length) }}
+												onBlur={() => {
+													row[col].editing = false
+													setData([...data])
+												}}
+												onChange={(event) => {
+													const matchNewline = /\r|\n/.test(event.target.value[event.target.value?.length - 1])
+													if (matchNewline && col == 7) {
+														row[col].value = event.target.value + moment(new Date()).format('DD.MM') + ' '
+													} else {
+														row[col].value = event.target.value
+													}
+													setData([...data])
+												}}
+											/>
+										) : (
+											<div style={{ fontSize: 18, lineHeight: '20px', paddingTop: 6, paddingBottom: 6, whiteSpace: 'pre-wrap', fontFamily: "Times New Roman" }}>{`${(row[col]?.value && ('' + row[col]?.value).trim()) || ''}`}</div>
+										)
+									}
+								</div>
+							</div>
+						))}
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
+export default App
