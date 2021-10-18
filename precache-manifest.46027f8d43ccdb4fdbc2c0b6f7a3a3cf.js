3536c76df72f805aa32232949e9e9a538660e8ef
haolv <haolv@solazu.com>
2021-10-18 23:15:00 +0700

    Updates

---
+++ precache-manifest.46027f8d43ccdb4fdbc2c0b6f7a3a3cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032179e7bee0b043b3ecf09e9e731cd0",
+    "url": "/inventory-management/index.html"
+  },
+  {
+    "revision": "87800f7e480d92cf6975",
+    "url": "/inventory-management/static/css/main.3bd20101.chunk.css"
+  },
+  {
+    "revision": "f771a7d6afe33ba35312",
+    "url": "/inventory-management/static/js/2.17e314ba.chunk.js"
+  },
+  {
+    "revision": "87800f7e480d92cf6975",
+    "url": "/inventory-management/static/js/main.0465c067.chunk.js"
+  },
+  {
+    "revision": "815eef29b19763c31e6b",
+    "url": "/inventory-management/static/js/runtime-main.a82034ce.js"
+  }
+]);
